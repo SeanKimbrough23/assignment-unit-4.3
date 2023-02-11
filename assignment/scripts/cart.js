d96b97ce9e6a8cd3e8a44f2a797f095f7408d3bb
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-02-11 13:05:29 -0600

    listItems function works

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -6,14 +6,27 @@ function addItem (item) {
 let newItem = basket.push(item)
 return true;
 }
-console.log('Add new item', addItem('fish'));
+console.log('Add new item (expect true)', addItem('fish'));
+console.log('Add new item (expect true)', addItem('peanuts'));
+console.log('Add new item (expect true)', addItem('blanket'));
+console.log('Add new item (expect true)', addItem('airpods'));
+console.log('Add new item (expect true)', addItem('book'));
+console.log('New Basket is now:' , basket);
+
 
 function listItems () {
     for (let i=0; i<basket.length; i++){
-    let newItem = basket [i];
-    console.log('Item added to Basket', newItem);
+    return basket[i];
+    }
+}   
+
+console.log('Item in Basket:', basket[0]);
+console.log('Item in Basket:', basket[1]);
+console.log('Item in Basket:', basket[2]);
+console.log('Item in Basket:', basket[3]);
+console.log('Item in Basket:', basket[4]);
+
+
+function empty (){
+    return basket = [];
 }
-}
-
-
-
