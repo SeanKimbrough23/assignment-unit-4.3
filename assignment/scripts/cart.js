6f5e8f7904ef1f2ce0675088b6c99ec77fd3cec0
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-02-11 12:32:30 -0600

    addItem to basket array works

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,5 +1,19 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+function addItem (item) {
+let newItem = basket.push(item)
+return true;
+}
+console.log('Add new item', addItem('fish'));
+
+function listItems () {
+    for (let i=0; i<basket.length; i++){
+    let newItem = basket [i];
+    console.log('Item added to Basket', newItem);
+}
+}
+
 
 
