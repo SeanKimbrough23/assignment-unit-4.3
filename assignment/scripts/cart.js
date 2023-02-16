73e5334381ad3fa68d7cb29ee0789fb59ec3b836
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-02-16 06:38:04 -0600

    added comments to code and teacher corrections

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,69 +1,71 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-let basket = [];
+let basket = []; // basket is set to an empty array
 function addItem (item) {
 let newItem = basket.push(item)
-return true;
+return true; // created a function addItem which takes an input newItem and adds it to basket array
 }
-console.log('Add new item (expect true)', addItem('fish'));
-console.log('Add new item (expect true)', addItem('peanuts'));
-console.log('Add new item (expect true)', addItem('blanket'));
-console.log('Add new item (expect true)', addItem('airpods'));
-console.log('Add new item (expect true)', addItem('book'));
+console.log('Add new item (expect true)', addItem('fish')); //added fish to array
+console.log('Add new item (expect true)', addItem('peanuts')); // added peanuts to array
+console.log('Add new item (expect true)', addItem('blanket')); // added blanket to array
+console.log('Add new item (expect true)', addItem('airpods')); // added airpods to array
+console.log('Add new item (expect true)', addItem('book')); // added book to array
 console.log('New Basket is now:' , basket);
 
 
-function listItems () {
+function listItems () { //created a function listItems that lists all items currenty in basket array
     for (let i=0; i<basket.length; i++){
-    return basket[i];
+    return console.log(basket[i]);
     }
 }   
 
-console.log('Item in Basket:', basket[0]);
-console.log('Item in Basket:', basket[1]);
-console.log('Item in Basket:', basket[2]);
-console.log('Item in Basket:', basket[3]);
-console.log('Item in Basket:', basket[4]);
+console.log('Item in Basket:', basket[0]); // logged the array item fish
+console.log('Item in Basket:', basket[1]); // logged the array item peanuts
+console.log('Item in Basket:', basket[2]); // logged the array item blanket
+console.log('Item in Basket:', basket[3]); // logged the array item airpods
+console.log('Item in Basket:', basket[4]); // logged the array item book
 
 
-function empty (){
-    return basket.length=[];
+function empty (){ // created a function empty 
+    return basket.length=0; // when function is called it should empty that array to 0 items
 }
-console.log(empty(basket));
-console.log('basket is?', basket);
-console.log('Added airpods, expect true' , addItem('airpods'));
-console.log('Added nikes, expect true' , addItem('nikes'));
-console.log('Added candy, expect true' , addItem('candy'));
-console.log('Added fruit, expect true', addItem('fruit'));
-console.log('Added meat, expect true', addItem('meat'));
-console.log(listItems());
-console.log('Basket is now' , basket);
+console.log(empty(basket.length)); // logged the basket length after it is emptied
+console.log('basket is?', basket.length); //array length should be 0
+console.log('Added airpods, expect true' , addItem('airpods')); // used addItem function to add airpods to array
+console.log('Added nikes, expect true' , addItem('nikes')); // used addItem function to add nikes to array
+console.log('Added candy, expect true' , addItem('candy')); // used addItem function to add candy to array
+console.log('Added fruit, expect true', addItem('fruit')); // used addItem function to add fruit to array
+console.log('Added meat, expect true', addItem('meat')); // used addItem function to add meat to array
+console.log('Added donuts, expect true', addItem('donuts'));// used addItem function to add donuts to array
+console.log(listItems()); // console should list all 6 items in array
+console.log('Basket is now' , basket.length); // array = 6
 // Stretch Goals////
-const maxItems = 5;
+const maxItems = 5; // created global variable maxItems = 5
 //let (basket.length)= maxItems;
 
 
-function isFull () {
-   (basket.length)= maxItems;
+function isFull () {// created function isFull
+   (basket.length)=== maxItems; // set basket.length is set to maxItems which is 5
 for (let i=0; i<basket.length; i++){
-    if ( basket.length < maxItems ){
+    if ( basket.length < maxItems ){ // if basket array is less than 5 return false
         return false ;
-    } else if (basket.length >= maxItems ){
+    } else if (basket.length >= maxItems ){ // if basket array is greater than or equal to 5 return true
         return true;
+    } else {
+        console.log('Basket is full', isFull(-1));
     }
 }
 }
 
-function removeItem() {
-    if (basket.indexOf(baskets) === 0) {
-      console.log(`1st item in array: ${basket}`);
+function removeItem() { // created function removeItem
+    if (basket.indexOf(basket) === 0) {
+      console.log(`1st item in array: ${basket}`); //logged 1st item in basket
     } else {
-        console.log(`${baskets} already exists in the collection.`);
+        console.log(`${basket} already exists in the collection.`);
     }
   }
-  
-  
+
 
 console.log('Basket is full', isFull(basket.length+6));
 console.log('is basket full?', isFull(basket.length));
